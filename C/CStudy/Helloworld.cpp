8ac33780312b399dfc6ef5c2d2978587cab2b8be
YongMin Son <76940705+F91-plus@users.noreply.github.com>
2025-02-14 17:31:28 +0900

    2025.02.14_hellow_world

---
+++ C/CStudy/Helloworld.cpp
@@ -0,0 +1,19 @@
+// 주석입니다.
+
+/*
+	작성일 : 2025.02.14
+	작성자 : 손용민
+	주제 : Hello World
+*/
+
+#include <stdio.h> // 라이브러리 사용 - 입출력 기능
+//#include : 포합하겠다
+//stdio : 표준 입출력을
+//h : header
+
+int main() { // 이 솔루션의 메인 실행 정의
+	printf("Hello World"); // 값을 출력한다.
+
+	return 0; // 반환값
+}
+
